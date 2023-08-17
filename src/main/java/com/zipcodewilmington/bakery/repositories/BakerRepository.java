b4b3cab2e7d3460a7a3a420a4abe15fd094c388e
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-16 20:53:43 -0400

    My annotation import was wrong that was hard

--- src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
@@ -2,6 +2,10 @@ package com.zipcodewilmington.bakery.repositories;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+
 
 public interface BakerRepository extends CrudRepository<Baker, Long> {
 }
--- src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
@@ -2,6 +2,10 @@ package com.zipcodewilmington.bakery.repositories;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+
 
 public interface MuffinRepository extends CrudRepository<Muffin, Long> {
 }
